a37d5c33bc8cfd0fccda35fb8a07b99c29c642a4
Nedramrez <zermarden@gmail.com>
2022-06-14 15:33:09 +0600

    Add navigation menu and style

---
+++ script.js
@@ -0,0 +1,8 @@
+function mobileMenu() {
+  var x = document.getElementById("mobMenu");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
