171cd89ddc82bdb45a4a8d14769685ea536e52d1
Nedramrez <zermarden@gmail.com>
2022-06-14 22:01:29 +0600

    Fix linters' errors 3

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // eslint-disable-next-line no-unused-vars
 function mobileMenu() {
-  let x = document.getElementById('mobMenu');
+  const x = document.getElementById('mobMenu');
   if (x.style.display === 'block') {
     x.style.display = 'none';
   } else {
