fca7830b25f752b5d78f64259d6d1c466fe4f7b7
Nedramrez <zermarden@gmail.com>
2022-06-21 17:25:16 +0600

    Initial commit: just a beginning

--- script.js
+++ script.js
@@ -8,49 +8,12 @@ function mobileMenu() {
   }
 }
 
-// eslint-disable-next-line no-unused-vars
-function detailsPp() {
-  const x = document.getElementById('pp-details');
-  if (x.style.display === 'block') {
-    x.style.display = 'none';
-  } else {
-    x.style.display = 'block';
-  }
-}
+//The details popup window
 
-const allCards = {
-  firstCard: {
-    name: 'Tonic',
-    image: '.assets/desktop-snapshoot-1.svg',
-    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
-    technologies: ['html', 'css', 'javaScript', 'github', 'ruby', 'Bootstraps'],
-    seeLive: 'https://nedramrez.github.io/portfolio/',
-    seeSource: 'https://github.com/nedramrez/portfolio',
-  },
-  secondCard: {
-    name: 'Tonic',
-    image: '.assets/desktop-snapshoot-1.svg',
-    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
-    technologies: ['html', 'css', 'javaScript', 'github', 'ruby', 'Bootstraps'],
-    seeLive: 'https://nedramrez.github.io/portfolio/',
-    seeSource: 'https://github.com/nedramrez/portfolio',
-  },
-  thirdCard: {
-    name: 'Tonic',
-    image: '.assets/desktop-snapshoot-1.svg',
-    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
-    technologies: ['html', 'css', 'javaScript', 'github', 'ruby', 'Bootstraps'],
-    seeLive: 'https://nedramrez.github.io/portfolio/',
-    seeSource: 'https://github.com/nedramrez/portfolio',
-  },
-  fourthCard: {
-    name: 'Tonic',
-    image: '.assets/desktop-snapshoot-1.svg',
-    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
-    technologies: ['html', 'css', 'javaScript', 'github', 'ruby', 'Bootstraps'],
-    seeLive: 'https://nedramrez.github.io/portfolio/',
-    seeSource: 'https://github.com/nedramrez/portfolio',
-  },
-};
+const works = document.querySelector('works');
 
-window.localStorage.setItem('allCards', JSON.stringify(allCards));
+const cardWorks = document.createElement('div');
+cardWorks.classList.add('card-works');
+cardWorks.textContent = 'TETETETETETET';
+
+works.append(cardWorks);
